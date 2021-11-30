2ff234630f7fe027d13570041cc7eec4cfd74659
kerev <backun-anatoly@yandex.ru>
2021-11-30 18:42:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// Создание экземпляра класса DOMParser
+const parser = new DOMParser();
+
+// XML, который пасрить
+const xmlString = `
+<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>35</age>
+    <prof>teacher</prof>
+  </student>
+  <student>
+    <name lang="ru">
+      <first>Петр</first>
+      <second>Петров</second>
+    </name>
+    <age>58</age>
+    <prof>driver</prof>
+  </student>
+</list>`;
+
+// парсинг XML
+const xmlDOM = parser.parseFromString(xmlString, "text/xml");
+// получение всех DOM-нод
+const listNode = xmlDOM.querySelector("list");
+const studenstNode = xmlDOM.querySelectorAll("student");
+const listArr = [];
+// ----------------------------------------------
+
+for (let i = 0; i < studenstNode.length; i++) {
+  const first_nameNode = listNode.children[i].querySelector("first");
+  const second_nameNode = listNode.children[i].querySelector("second");
+  const ageNode = listNode.children[i].querySelector("age");
+  const profNode = listNode.children[i].querySelector("prof");
+  const nameNode = listNode.children[i].querySelector("name");
+  const langAttr = nameNode.getAttribute("lang");
+
+  listArr.push({
+    name: `${first_nameNode.textContent} ${second_nameNode.textContent}`,
+    age: Number(ageNode.textContent),
+    prof: profNode.textContent,
+    lang: langAttr,
+  });
+}
+const result = {
+  list: listArr,
+};
+console.log('result', result);
